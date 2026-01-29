c65d70071e4571f7d937b1ed5fa46d101180a8af
李康敏 <likangmin163@163.com>
2026-01-29 17:17:02 +0800

    修改部分信息

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 module.exports = {
   admin: {
     username: process.env.ADMIN_USERNAME || 'admin',
-    password: process.env.ADMIN_PASSWORD || '123456'
+    password: process.env.ADMIN_PASSWORD || '67890'
   },
   server: {
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'nav-item-jwt-secret-2024-secure-key'
+    jwtSecret: process.env.JWT_SECRET || 'nav-website-jwt-secret-2026-secure-key'
   }
-}; 
+};
